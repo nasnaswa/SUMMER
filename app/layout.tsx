503ab0b3f8dc777b18bf5bd4429d233959deb043
Naswa <naswalia31@gmail.com>
2025-01-11 14:24:40 +0700

    naswa: buat header dan ubah bg body

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>/</>
+    <html lang="id" data-theme="light">
+      <body className="bg-gray-100">
+        {/* AREA HEADER */}
+        <header className="bg-[#bed4d2] text-[#8d241d] py-4 px-6 shadow-md">
+            <h1 className="text-2xl font-bold font-sans">Toko Roti Permata</h1>
+        </header>
+        {/* AREA HEADER */}
+      </body>
+    </html>
   );
 }
