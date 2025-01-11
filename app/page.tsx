e703e650e6f607af17156e25b18e919c3fc58780
nabilachyni <email@nabilapc06@gmail.com>
2025-01-11 16:51:21 +0700

    nabilla: buat heading katalog

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div  className="max-w-4xl mx-auto p-5 bg-gray-50 text-gray-800">
+    <h1 className="text-3xl font-bold font-sans text-[#8d241d] text-center mb-4">Katalog Toko Roti</h1>
+
+    </div>
   );
 }
