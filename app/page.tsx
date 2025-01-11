4d47f2e1dc46fd9304a154a62275868c87879025
nabilachyni <email@nabilapc06@gmail.com>
2025-01-11 18:07:50 +0700

    nabilla: menampilkan produk

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,23 @@ export default function Home() {
         Selamat datang di Toko Roti Kami! Kami menawarkan berbagai pilihan roti lezat 
         yang dibuat dengan bahan berkualitas. Nikmati rasa yang luar biasa di setiap gigitan!
       </p>
-    
-
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="border border-gray-300 rounded-lg shadow-md p-4 text-center"> 
+        <Image src="/roti-bakso.jpg" alt="Roti Bakso" width={200} height={200} className="mx-auto mb-3" />
+        <h2 className="text-xl font-semibold font-sans mb-2">Roti Bakso</h2>
+          <p className="text-gray-500">Harga: Rp11.000</p>
+      </div>
+      <div  className="border border-gray-300 rounded-lg shadow-md p-4 text-center"> 
+      <Image src="/roti-donat.jpg" alt="Donat" width={200} height={200} className="mx-auto mb-3" />
+          <h2 className="text-xl font-semibold font-sans  mb-2">Donat</h2>
+          <p className="text-gray-500">Harga: Rp5.000</p>
+      </div >
+      <div className="border border-gray-300 rounded-lg shadow-md p-4 text-center">
+      <Image src="/roti-bombolini.jpg" alt="Bombolini" width={200} height={200} className="mx-auto mb-3" />
+          <h2 className="text-xl font-semibold font-sans  mb-2">Bombolini</h2>
+          <p className="text-gray-500">Harga: Rp30.000</p>
+      </div>
+      </div>
     </div>
   );
 }
