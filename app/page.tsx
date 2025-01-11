74ee29c5c2511b364d180e97a7855c1387303ce9
nabilachyni <email@nabilapc06@gmail.com>
2025-01-11 17:11:32 +0700

    nabilla: namba deskripsi

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <div  className="max-w-4xl mx-auto p-5 bg-gray-50 text-gray-800">
     <h1 className="text-3xl font-bold font-sans text-[#8d241d] text-center mb-4">Katalog Toko Roti</h1>
+    <p className="text-center text-gray-600 mb-8">
+        Selamat datang di Toko Roti Kami! Kami menawarkan berbagai pilihan roti lezat 
+        yang dibuat dengan bahan berkualitas. Nikmati rasa yang luar biasa di setiap gigitan!
+      </p>
+    
 
     </div>
   );
